b8575b894b8e314af9d4f3e66b2187443d234397
Anderson Freitas <mfderson@gmail.com>
2023-03-09 22:11:26 -0300

    implement UserDetails and UserDetailsService to basic auth with database

--- src/main/kotlin/com/ead/authuser/configs/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/ead/authuser/configs/security/WebSecurityConfig.kt
@@ -2,18 +2,27 @@ package com.ead.authuser.configs.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-class WebSecurityConfig {
+class WebSecurityConfig(
+    val userDetailsServiceImpl: UserDetailsServiceImpl,
+    val authenticationEntryPoint: AuthenticationEntryPointImpl
+) {
 
     companion object {
         private const val AUTH_WHITELIST = "/auth/**"
@@ -22,23 +31,30 @@ class WebSecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .httpBasic().and()
+            .httpBasic()
+            .authenticationEntryPoint(authenticationEntryPoint)
+            .and()
             .authorizeHttpRequests()
             .antMatchers(AUTH_WHITELIST).permitAll()
+            .antMatchers(HttpMethod.GET, "/users/**").hasRole("STUDENT")
             .anyRequest().authenticated().and()
             .csrf().disable()
         return http.build()
     }
 
     @Bean
-    fun userDetailsService(): InMemoryUserDetailsManager {
-        val user = User
-            .withUsername("admin")
-            .password(passwordEncoder().encode("123456"))
-            .roles("ADMIN")
-            .build()
-        return InMemoryUserDetailsManager(user)
+    fun authenticationProvider(): DaoAuthenticationProvider {
+        val authProvider = DaoAuthenticationProvider()
+
+        authProvider.setUserDetailsService(userDetailsServiceImpl)
+        authProvider.setPasswordEncoder(passwordEncoder())
+
+        return authProvider
     }
+
+    @Bean
+    fun authenticationManager(authConfig: AuthenticationConfiguration): AuthenticationManager =
+        authConfig.authenticationManager
 
     @Bean
     fun passwordEncoder() = BCryptPasswordEncoder()
